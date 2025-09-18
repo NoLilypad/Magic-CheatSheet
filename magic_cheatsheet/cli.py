7e734ac09da2dfa657b3e997139ab41bdbf0c562
Rubens TORRES-LACAZE <rubens.torres-lacaze@telecom-sudparis.eu>
2025-09-18 16:07:04 +0200

    Ready to write main app

--- magic_cheatsheet/cli.py
+++ magic_cheatsheet/cli.py
@@ -1,7 +1,1 @@
-def main():
-    import sys
-    if len(sys.argv) < 2:
-        print("Usage: magic-cheatsheet <commande>")
-        sys.exit(1)
-    command = sys.argv[1]
-    print(f"Generating cheatsheet for : {command}")
+from .main import main
---
+++ magic_cheatsheet/main.py
@@ -0,0 +1,26 @@
+
+import argparse
+import sys
+from .app import app
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate a cheatsheet for a given command.")
+    parser.add_argument("command", nargs="?", help="The command to generate a cheatsheet for")
+    parser.add_argument("--detailed", action="store_true", help="Show a detailed cheatsheet")
+    parser.add_argument("--version", action="store_true", help="Show the version of Magic-CheatSheet")
+    parser.add_argument("--config", action="store_true", help="Configuration of the tool")
+
+    args = parser.parse_args()
+
+    if args.version:
+        print("Magic-CheatSheet version 1.0.0")
+        sys.exit(0)
+
+    if args.configuration:
+        pass
+
+    if not args.command:
+        print("Usage: magic-cheatsheet <command> [--detailed] [--version]")
+        sys.exit(1)
+
+    app(args.command, args.detailed)
