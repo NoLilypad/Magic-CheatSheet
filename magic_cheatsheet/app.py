23b2973529b92847c1d9d4ec19e5d6d1c0cbbe57
Rubens TORRES-LACAZE <rubens.torres-lacaze@telecom-sudparis.eu>
2025-09-18 17:16:17 +0200

    Added prompt generation

--- magic_cheatsheet/app.py
+++ magic_cheatsheet/app.py
@@ -1,12 +1,41 @@
 from .manage_config import get_config
+import platform
+
+def get_prompt(command: str, detailed: bool):
+    os_name = platform.system()
+    if detailed:
+        details = "Detail a lot"
+    else:
+        details = ""
+    prompt = f"""# Role
+You are an AI agent that generates cheatsheets for cli commands. 
+
+# Mission
+Based on this user command : {command} 
+generate a cheatsheet of this CLI command. 
+
+# Response format
+{details}
+
+# Data
+User's OS : {os_name}"""
+    return prompt
+
+
 
 def app(command: str, detailed: bool = False):
     config = get_config()
     print(f"Loaded config: {config}")
     print(f"Generating cheatsheet for: {command}")
     
-    if detailed:
-        print("(Detailed mode enabled)")
-    # Here will be the core logic for generating the cheatsheet
+    prompt = get_prompt(command, detailed)
+
+    if config.provider == "mistral":
+        pass
+
+    if config.provider == "ollama":
+        pass
+
+
 
 
